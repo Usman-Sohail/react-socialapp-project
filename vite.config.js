a58281e4aedf7b529706f44b1a0d80ba113c929b
Usman-Sohail <usman.sohail1212@gmail.com>
2025-10-31 19:14:18 +0500

    Fix: Vercel deployment routing for React Router

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
-    host: true,
-    port: 5173,
-    allowedHosts: ["humanely-unpensioning-marilyn.ngrok-free.dev"],
+    allowedHosts: ["*"],
   },
 });
